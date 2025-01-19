acdc54c0482fa22b5ae54f75a4f87a8845dd6c62
Ian Gathua <gathuambui@gmail.com>
2025-01-20 00:52:45 +0300

    Created react app and components

---
+++ e-commerce-cart-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import products from './products'
+import Product from './components/Product';
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>E-Commerce Cart</h2>
+      <ul>
+        {products.map((prodduct) => (
+          <li key = {products.id}>
+            {products.name} - Ksh.{products.price}
+          </li>
+        ))}
+      </ul>
+ 
+ 
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce-cart-app/src/components/ProductsList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Product from "./Product";
+
+const ProductList = ({ products, addToCart}) => {
+
+}
+
+export default ProductList
